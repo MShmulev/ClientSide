d3c33592f6251604173f6ec8e1fd429acf4fb3ac
MShmulev <117279651+MShmulev@users.noreply.github.com>
2023-01-16 19:48:52 +0200

    Add files via upload

--- FinalProject/javascript.js
+++ FinalProject/javascript.js
@@ -1,60 +1,63 @@
-const actions = document.querySelector('.actions');
-	const ans = document.querySelector('.ans');
-	console.log(actions);
-	console.log(ans);
-	let expression = '';
-	let a=0;
-	actions.addEventListener('click', (e) => {
-		console.log(e.target);
-		const value = e.target.dataset['value'];
+function addChar(input, character) {
+	if(input.value == null || input.value == "0")
+		input.value = character
+	else
+		input.value += character
+}
  
-		if(value !== undefined) {
-			// I'm good to go.
-			if(value == 'ce') {
-				expression = '';
-				ans.value = 0;
-				return true;
-			}
-			else if(value == 'x^2'){
-				expression =square();
-			}
-			
-			else if(value == 'radic'){
-				expression = Math.sqrt(expression);
-			}
-			else if(value == 'log'){
-				expression = Math.log(expression);
-			}
-			else if(value == 'sin'){
-				expression = Math.sin(expression);
-			}
-			else if(value == 'cos'){
-				expression = Math.cos(expression);
-			}
-			else if(value == 'tan'){
-				expression = Math.tan(expression);
-			}
+function cos(form) {
+	form.display.value = Math.cos(form.display.value);
+}
  
-			else if(value == '=') {
-				const answer = eval(expression);
-				expression = answer;
-				
-			} else {
-				expression += value;
+function sin(form) {
+	form.display.value = Math.sin(form.display.value);
+}
+ 
+function tan(form) {
+	form.display.value = Math.tan(form.display.value);
+}
+ 
+function sqrt(form) {
+	form.display.value = Math.sqrt(form.display.value);
+}
+ 
+function ln(form) {
+	form.display.value = Math.log(form.display.value);
+}
+ 
+function exp(form) {
+	form.display.value = Math.exp(form.display.value);
+}
+ 
+function deleteChar(input) {
+	input.value = input.value.substring(0, input.value.length - 1)
+}
+ 
+function changeSign(input) {
+	if(input.value.substring(0, 1) == "-")
+		input.value = input.value.substring(1, input.value.length)
+	else
+		input.value = "-" + input.value
+}
+ 
+function compute(form) {
+	form.display.value = eval(form.display.value)
+}
+ 
+function square(form) {
+	form.display.value = eval(form.display.value) * eval(form.display.value)
+}
+ 
+function checkNum(str) {
+	for (var i = 0; i < str.length; i++) {
+		var ch = str.substring(i, i+1)
+		if (ch < "0" || ch > "9") {
+			if (ch != "/" && ch != "*" && ch != "+" && ch != "-" && ch != "."
+				&& ch != "(" && ch!= ")") {
+				alert("invalid entry!")
+				return false
+				}
 			}
- 
-			if(expression == undefined) {
-				expression = '';
-				ans.value = 0;
-			} else {
-				ans.value = expression;
-			}
-			// expression += value;
- 
- 
 		}
- 
-	});
-	const square =()=> {
-			return eval(expression*expression);
-	}
+		return true
+}
